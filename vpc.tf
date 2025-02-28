5f2d229cbc9dd1e91a65ac1adaf7a25e317dbf55
rajdattb <Rajdatt@users.noreply.github.com>
2025-03-01 00:51:05 +0530

    Updated vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,13 @@
+provider "aws" {
+  region = "us-east-1"  # Set the region
+}
+
+# Create VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Create Private Subnets
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
@@ -14,6 +20,7 @@ resource "aws_subnet" "private_subnet_2" {
   availability_zone = "us-east-1b"
 }
 
+# Security Group for MongoDB
 resource "aws_security_group" "mongo_sg" {
   vpc_id = aws_vpc.main.id
 
@@ -21,14 +28,87 @@ resource "aws_security_group" "mongo_sg" {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] # Only internal access
+    cidr_blocks = ["10.0.0.0/16"] # Only allow internal access
   }
 }
 
-resource "aws_vpc_endpoint" "lambda_mongo" {
+# Security Group for VPC Endpoints
+resource "aws_security_group" "vpc_endpoints_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]  # Allow private subnet access
+  }
+}
+
+# Create Interface VPC Endpoints for AWS Services
+resource "aws_vpc_endpoint" "lambda" {
   vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.us-east-1.s3"
+  service_name      = "com.amazonaws.us-east-1.lambda"
   vpc_endpoint_type = "Interface"
   subnet_ids        = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-  security_group_ids = [aws_security_group.mongo_sg.id]
+  security_group_ids = [aws_security_group.vpc_endpoints_sg.id]
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.ecr.api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  security_group_ids = [aws_security_group.vpc_endpoints_sg.id]
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  security_group_ids = [aws_security_group.vpc_endpoints_sg.id]
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.logs"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  security_group_ids = [aws_security_group.vpc_endpoints_sg.id]
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create NAT Gateway for Private Subnets
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.private_subnet_1.id
+}
+
+# Route Table for Private Subnets (NAT Gateway)
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private_subnet_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
 }
